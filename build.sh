1708a595dcb3f9c62a9307062bc08c9eeefe4f04
Dominique Orban <dominique.orban@gmail.com>
2013-08-23 21:04:19 -0400

    Initial draft.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Pip stuff.
+pip install ffc
+
+#   `pip install fiat` currently fails.
+#   For the time being, grab FIAT from BitBucket.
+pip install -e git+https://bitbucket.org/fenics-project/fiat.git#egg=fiat
+pip install instant
+pip install ufl
+
+# Homebrew stuff.
+# 1. Prerequisites
+brew install swig
+brew install boost --with-mpi --without-single
+
+brew tap homebrew/science
+brew install armadillo
+brew install scotch
+
+#2. Install FEniCS.
+brew install ufc
+brew install dolfin
