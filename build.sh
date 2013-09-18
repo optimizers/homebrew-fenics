a594479f3e7cd879f6f36f64ef78a11b6b9f2d81
Dominique Orban <dominique.orban@gmail.com>
2013-09-18 10:40:39 -0400

    Build devel version of everything in FEniCS.

--- build.sh
+++ build.sh
@@ -4,13 +4,32 @@
 brew install python
 
 # Pip stuff.
-pip install ffc
+
+## FFC
+#pip install ffc
+pip install git+https://bitbucket.org/fenics-project/ffc.git
+
+## FIAT
+# FIAT requires ScientificPython, though it doesn't say anywhere...
+pip install https://sourcesup.renater.fr/frs/download.php/4153/ScientificPython-2.9.2.tar.gz
 
 #   `pip install fiat` currently fails.
 #   For the time being, grab FIAT from BitBucket.
-pip install -e git+https://bitbucket.org/fenics-project/fiat.git#egg=fiat
-pip install instant
-pip install ufl
+pip install git+https://bitbucket.org/fenics-project/fiat.git
+
+## INSTANT
+#pip install instant
+pip install git+https://bitbucket.org/fenics-project/instant.git
+
+## UFL
+# pip install ufl
+pip install git+https://bitbucket.org/fenics-project/ufl.git
+
+## PLY (required to Swig stuff.)
+pip install ply
+
+## SPHINX
+pip install sphinx
 
 # Homebrew stuff.
 # 1. Prerequisites
@@ -20,9 +39,32 @@ brew install boost --with-mpi --without-single
 brew tap homebrew/science
 brew install armadillo
 brew install cgal
+brew install hdf5 --enable-parallel
+
 brew install petsc
-brew install scotch
+pip install petsc4py
+
+brew install parmetis
+
+brew install qt
+
+brew install https://raw.github.com/michaelwimmer/homebrew-science/e1806b4a754ef0575da2d798be6cce2f222c1bb8/scotch5.rb
+#brew install scotch  # Scotch 6 is not compatible with Pastix.
+brew install pastix
+
+brew install slepc
+pip install slepc4py
+
+brew install sphinx
+
+brew install tao
+pip install tao4py
+
+brew install vtk --with-qt --with-python
+
+# ! The trilinos formula has been patched.
+brew install trilinos --with-boost --with-scotch
 
 #2. Install FEniCS.
 brew install ufc
-brew install dolfin
+#brew install dolfin
