c48339327053e56275fa71eb7d831bf0b6cdf072
Dominique Orban <dominique.orban@gmail.com>
2013-09-18 21:04:35 -0400

    TAO pre-release 2.2

--- tao.rb
+++ tao.rb
@@ -2,22 +2,21 @@ require 'formula'
 
 class Tao < Formula
   homepage 'http://www.mcs.anl.gov/research/projects/tao'
-  url 'http://www.mcs.anl.gov/research/projects/tao/download/tao-2.1-p2.tar.gz'
-  sha1 '5dcee962499aad9ccaa4ee968d96c70bdfb79955'
-  version '2.1-p2'
+  url 'http://www.mcs.anl.gov/tao/download/tao-2.2-pre1.tar.gz'
+  sha1 'e4b4790cf18d39f2b0b9810d3478163634fce66a'
+  version '2.2'
 
-  # TAO 2.1 depends on PETSc 3.3
-  depends_on 'petsc' => :build
+  depends_on 'petsc' => :build  # TAO 2.2 depends on PETSc 3.4
   depends_on :fortran
   env :std  # Tests do not pass without this...
 
   def install
-
+    ENV.deparallelize
     ENV['TAO_DIR'] = Dir.getwd
     ENV['PETSC_DIR'] = Formula.factory('petsc').prefix
-    # Contrary to the TAO documentation, leave PETSC_ARCH undefined.
+    # Contrary to the TAO documentation,
+    # it's important to leave PETSC_ARCH undefined.
     system "make all"
-    system "make tao_shared"
     system "make tao_testexamples"
     system "make tao_testfortran"
     ohai 'Test results are in ~/Library/Logs/Homebrew/tao. Please check them.'
