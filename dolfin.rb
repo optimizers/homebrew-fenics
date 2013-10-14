72b03584cd10e1e32a7d39236356ae39a60494ef
Dominique Orban <dominique.orban@gmail.com>
2013-10-14 09:34:56 -0400

    List all deps explicitly.

--- dolfin.rb
+++ dolfin.rb
@@ -4,27 +4,42 @@ class Dolfin < Formula
   homepage 'https://bitbucket.org/fenics-project/dolfin'
   url ''
   head 'https://bitbucket.org/fenics-project/dolfin.git'
-  version '1357c06'
 
-  # depends_on 'metis'    => :build
-  depends_on 'parmetis' => :build
-  depends_on 'swig'     => :build
-  depends_on 'cmake'    => :build
-  depends_on 'petsc'    => :build
-  # depends_on 'scotch5'  => :build
-  depends_on 'scotch'   => :build    # Must be Scotch >= 6
-  depends_on 'pastix'   => :build
-  depends_on 'mtl'      => :build
-  depends_on 'cppunit'  => :build
-  depends_on 'hdf5'     => 'enable-parallel'
+  depends_on :mpi => [:cc, :cxx, :f90]
+
+  # List all dependencies explicitly for completeness.
+  depends_on 'boost'     => ['--with-mpi', '--without-single']
+  depends_on 'armadillo' => :build
+  depends_on 'cgal'      => :build
+  depends_on 'parmetis'  => :build
+  depends_on 'swig'      => :build
+  depends_on 'cmake'     => :build
+  depends_on 'petsc'     => :build
+  depends_on 'slepc'     => :build
+  depends_on 'scotch'    => :build    # Must be Scotch >= 6
+  depends_on 'pastix'    => :build
+  depends_on 'tao'       => :build
+  depends_on 'trilinos'  => ['--with-boost', '--with-scotch']
+  depends_on 'mtl'       => :build
+  depends_on 'cppunit'   => :build
+  depends_on 'hdf5'      => 'enable-parallel'
+  depends_on 'suite-sparse' => :build
+  depends_on 'qt'        => :build
+  depends_on 'vtk'       => ['--with-qt', '--with-python']
+  depends_on 'sphinx'    => :build
 
   depends_on :fortran
   depends_on :python
 
   def install
+    if ENV.compiler == :clang
+      opoo 'OpenMP support will not be enabled. Use --use-gcc if you require OpenMP.'
+    end
+
     # ENV.j1  # if your formula's build system can't parallelize
     ENV['PETSC_DIR'] = Formula.factory('petsc').prefix
-    ENV['SCOTCH_DIR'] = Formula.factory('scotch5').prefix
+    ENV['PETSC_ARCH'] = 'arch-darwin-c-opt'
+    ENV['SCOTCH_DIR'] = Formula.factory('scotch').prefix
     ENV['PASTIX_DIR'] = Formula.factory('pastix').prefix
     ENV['SLEPC_DIR'] = Formula.factory('slepc').prefix
     ENV['PARMETIS_DIR'] = Formula.factory('parmetis').prefix
@@ -32,15 +47,12 @@ class Dolfin < Formula
     ENV['CPPUNIT_DIR'] = Formula.factory('cppunit').prefix
     ENV['CGAL_DIR'] = Formula.factory('cgal').prefix
 
-    # This seems necessary to find omp.h
-    ENV.append_to_cflags "-I#{Formula.factory('metis').include}"
-
     # This is necessary to discover CGAL.
     ENV.append_to_cxxflags "-frounding-math"
 
     mkdir 'build' do
       system "cmake", "..", *std_cmake_args
-      system "make", "install" # if this fails, try separate make/make install steps
+      system "make"
     end
   end
 end
