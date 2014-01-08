ea47c05755c0ab9febb38c6b4f86723b978b23d9
Luke Olson <luke.olson@gmail.com>
2014-01-08 12:49:56 -0600

    Creates blank formula for debugging

--- test.rb
+++ test.rb
@@ -1,6 +1,8 @@
 require 'formula'
 
 class Test < Formula
+  url 'http://downloads.sourceforge.net/project/synfig/releases/0.64.1/source/ETL-0.04.17.tar.gz'
+  sha1 '2ab2957140deaad90232533461513c5d425007bb'
 
   depends_on 'vtk5' => ['with-qt', 'with-python']
 
@@ -8,6 +10,7 @@ class Test < Formula
     depends_on 'sip'
     depends_on 'pyqt'
   end
+
 
   def install
     puts 'hi there'
