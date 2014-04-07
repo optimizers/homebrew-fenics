89cad2cdf65cf66a9971e958a72c1b00c01ba22f
Dominique Orban <dominique.orban@gmail.com>
2014-04-06 23:33:29 -0700

    UFC is now deprecated.
    The code for UFC is now contained in FFC. The formula for UFC should be
    removed once the new FFC is released.

--- ufc.rb
+++ ufc.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Ufc < Formula
   homepage "https://bitbucket.org/fenics-project/ufc"
-  url "https://bitbucket.org/fenics-project/ufc/downloads/ufc-2.3.0.tar.gz"
+  url "https://bitbucket.org/fenics-project/ufc-deprecated/downloads/ufc-2.3.0.tar.gz"
   sha1 "4cb7a9d80414894532e1a1dbe8ccdf42e5f563bb"
 
   depends_on :python
