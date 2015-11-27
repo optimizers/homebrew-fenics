568875c341ffefcde9b1461ad5f62e430a9121b8
Dominique Orban <dominique.orban@gmail.com>
2015-11-26 19:19:27 -0500

    ufl 1.6.0, audit fixes, test.

--- ufl.rb
+++ ufl.rb
@@ -1,16 +1,27 @@
-require 'formula'
-
 class Ufl < Formula
-  homepage 'https://bitbucket.org/fenics-project/ufl'
-  url 'https://bitbucket.org/fenics-project/ufl/downloads/ufl-1.4.0.tar.gz'
-  sha1 '4069e9477c07ae2841418bbdea6a4f2f19c870b7'
+  desc "DSL for FEM discretizations of variational forms"
+  homepage "https://bitbucket.org/fenics-project/ufl"
+  url "https://bitbucket.org/fenics-project/ufl/downloads/ufl-1.6.0.tar.gz"
+  sha256 "c75c4781e5104504f158cb42cd87aceffa9052e8e9db6e9764e6a5b6115d7f73"
+  head "https://bitbucket.org/fenics-project/ufl.git"
 
-  depends_on :python
-  depends_on 'numpy' => :python
+  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "numpy" => :python
 
   def install
     ENV.deparallelize
+    system "python", *Language::Python.setup_install_args(prefix)
+    pkgshare.install "demo"
+  end
 
-    system 'python', 'setup.py', 'install', "--prefix=#{prefix}"
+  test do
+    system "ufl-analyse", pkgshare/"demo/MixedMixedElement.ufl"
+    (testpath/"testufl.py").write <<-EOS.undent
+      from ufl.algorithms import load_ufl_file
+      filedata = load_ufl_file("#{pkgshare}/demo/MixedMixedElement.ufl")
+      forms = filedata.forms
+      elements = filedata.elements
+    EOS
+    system "python", testpath/"testufl.py"
   end
 end
