a8cdc4e5cd6e762e1034420255c2a3e575a9e015
Dominique Orban <dominique.orban@gmail.com>
2015-11-26 19:19:37 -0500

    instant 1.6.0, audit fixes, test.

--- instant.rb
+++ instant.rb
@@ -1,17 +1,26 @@
-require 'formula'
-
 class Instant < Formula
-  homepage 'https://bitbucket.org/fenics-project/instant'
-  url 'https://bitbucket.org/fenics-project/instant/downloads/instant-1.4.0.tar.gz'
-  sha1 '9f5c3f041cfde3005b22d1e3d8bd256b8e495b4e'
+  desc "inlining of C and C++ code in Python"
+  homepage "https://bitbucket.org/fenics-project/instant"
+  url "https://bitbucket.org/fenics-project/instant/downloads/instant-1.6.0.tar.gz"
+  sha256 "2347e0229531969095911fdb1de30bd77bdd7f81521ba84d81b1b4a564fc906c"
+  head "git clone https://bitbucket.org/fenics-project/instant.git"
 
-  depends_on 'swig'
-  depends_on :python
-  depends_on 'numpy' => :python
+  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "swig" => :build
+  depends_on "numpy" => :python
 
   def install
     ENV.deparallelize
+    system "python", *Language::Python.setup_install_args(prefix)
 
-    system 'python', 'setup.py', 'install', "--prefix=#{prefix}"
+    # Tests that require numpy.i just won't compile at this point.
+    pkgshare.install "test"
+  end
+
+  test do
+    cp_r opt_pkgshare/"test", testpath
+    cd testpath/"test" do
+      system "python", "run_tests.py"
+    end
   end
 end
