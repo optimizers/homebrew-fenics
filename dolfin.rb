bfa8095f83eba471e9e4cc82e39a0248f5f8105b
Dominique Orban <dominique.orban@gmail.com>
2015-11-29 12:20:57 -0500

    dolfin 1.6.0
    Not everything is correctly detected yet.

--- dolfin.rb
+++ dolfin.rb
@@ -1,71 +1,91 @@
-require 'formula'
-
 class Dolfin < Formula
-  homepage 'https://bitbucket.org/fenics-project/dolfin'
-  url 'https://bitbucket.org/fenics-project/dolfin/downloads/dolfin-1.4.0.tar.gz'
-  sha1 'b58cb5e4aaef0825ded1237ba6012d7aee63c152'
+  desc "C++/Python interface of FEniCS"
+  homepage "https://bitbucket.org/fenics-project/dolfin"
+  url "https://bitbucket.org/fenics-project/dolfin/downloads/dolfin-1.6.0.tar.gz"
+  sha256 "67eaac5fece6e71da0559b4ca8423156f9e99a952f0620adae449ebebb6695d1"
+  head "https://bitbucket.org/fenics-project/dolfin.git"
 
+  depends_on :python if MacOS.version <= :snow_leopard
   depends_on :fortran
-  depends_on :python
-
-  depends_on 'suite-sparse'
-  depends_on 'eigen'
-
-  depends_on 'cmake' => :build
-  depends_on 'cppunit' => :build
-  depends_on 'pkg-config' => :build
-  depends_on 'swig' => :build
-
-  depends_on 'numpy' => :python
-  depends_on 'ply' => :python
-
   depends_on :mpi => [:cc, :cxx, :f90, :recommended]
 
-  depends_on 'petsc-fenics' => :recommended if build.with? :mpi
-  depends_on 'slepc' => :recommended if build.with? :mpi
-  depends_on 'scotch' => :recommended if build.with? :mpi
-  depends_on 'pastix' => :recommended if build.with? :mpi
+  depends_on "cmake"      => :build
+  depends_on "cppunit"    => :build
+  depends_on "pkg-config" => :build
+  depends_on "swig"       => :build
 
-  depends_on 'parmetis' => :recommended if build.with? :mpi
-  depends_on 'hdf5' => ['enable-parallel', :recommended] if build.with? :mpi
-  depends_on 'boost' => ['without-single', 'with-mpi'] if build.with? :mpi
+  depends_on "eigen"
+  depends_on "suite-sparse"
+  depends_on "vtk"      => ["--with-qt"]
 
-  if build.without? :mpi
-    depends_on 'boost' => ['--without-single']
+  depends_on "numpy" => :python
+  depends_on "ply"   => :python
+
+  boost_deps = ["without-single"] + ((build.with? "mpi") ? ["with-mpi"] : [])
+  depends_on "boost"    => boost_deps
+
+  hdf5_deps = [:recommended] + ((build.with? "mpi") ? ["with-mpi"] : [])
+  depends_on "hdf5"     => hdf5_deps
+
+  depends_on "parmetis" => :recommended if build.with? "mpi"
+  depends_on "pastix"   => :recommended if build.with? "mpi"
+  depends_on "petsc"    => :recommended if build.with? "mpi"
+  depends_on "slepc"    => :recommended if build.with? "mpi"
+  depends_on "scotch"   => :recommended if build.with? "mpi"
+
+  trilinos_deps = [:recommended] + ((build.with? "mpi") ? [] : ["without-mpi"])
+  depends_on "trilinos" => trilinos_deps
+
+  depends_on "optimizers/fenics/fiat"
+  depends_on "optimizers/fenics/ufl"
+  depends_on "optimizers/fenics/ffc"
+  depends_on "optimizers/fenics/instant"
+
+  option "with-openmp"
+  needs :openmp if build.with? "openmp"
+
+  env :std # To properly detect PARMETIS.
+
+  def onoff(s, cond)
+    s + (cond ? "ON" : "OFF")
   end
 
-  depends_on 'vtk' => ['--with-qt']
-
-  depends_on 'fiat'
-  depends_on 'ufl'
-  depends_on 'ffc'
-  depends_on 'instant'
-
   def install
-    if ENV.compiler == :clang
-      opoo 'OpenMP support will not be enabled. Use --use-gcc if you require OpenMP.'
-    end
-
     ENV.deparallelize
-    ENV['PETSC_DIR'] = Formula.factory('petsc-fenics').prefix
-    ENV['PETSC_ARCH'] = 'arch-darwin-c-opt'
-    ENV['PARMETIS_DIR'] = Formula['parmetis'].prefix
-    ENV['AMD_DIR'] = Formula['suite-sparse'].prefix
-    ENV['CHOLMOD_DIR'] = Formula['suite-sparse'].prefix
-    ENV['UMFPACK_DIR'] = Formula['suite-sparse'].prefix
+    cmake_args = %W[-DCMAKE_INSTALL_PREFIX=#{prefix},
+                    -DBUILD_SHARED_LIBS:BOOL=ON,
+                    -DDOLFIN_ENABLE_PYTHON:BOOL=ON,
+                    -DDOLFIN_INSTALL_PYTHON_MODULE_DIR=#{lib}/python2.7/site-packages,
+                    -DDOLFIN_INSTALL_PYTHON_PURE_MODULE_DIR=#{lib}/python2.7/site-packages,
+                    -DDOLFIN_ENABLE_UMFPACK:BOOL=ON,
+                    -DUMFPACK_INCLUDE_DIRS=#{Formula["suite-sparse"].opt_include},
+                    -DDOLFIN_ENABLE_VTK:BOOL=ON,
+                    -DDOLFIN_ENABLE_CHOLMOD:BOOL=ON,
+                    -DCHOLMOD_INCLUDE_DIRS=#{Formula["suite-sparse"].opt_include},
+                 ]
 
-    # ENV['SLEPC_DIR'] = Formula.factory('slepc').prefix
-    # ENV['TAO_DIR'] = Formula['tao'].prefix
-    # ENV['SCOTCH_DIR'] = Formula.factory('scotch').prefix
-    # ENV['PASTIX_DIR'] = Formula.factory('pastix').prefix
-    # #ENV['TAO_DIR'] = Formula.factory('tao').prefix
-    # ENV['CPPUNIT_DIR'] = Formula.factory('cppunit').prefix
-    # ENV.append_to_cflags '-frounding-math'
+    ENV["CHOLMOD_DIR"] = "#{Formula["suite-sparse"]}.opt_prefix"
+    ENV["UMFPACK_DIR"] = "#{Formula["suite-sparse"]}.opt_prefix"
 
-    mkdir 'build' do
-      system 'cmake', '..', *std_cmake_args
-      system 'make'
-      system 'make', 'install'
+    cmake_args << onoff("-DDOLFIN_ENABLE_OPENMP:BOOL=", (build.with? "openmp"))
+    cmake_args << onoff("-DDOLFIN_ENABLE_HDF5:BOOL=", (build.with? "hdf5"))
+    cmake_args << onoff("-DDOLFIN_ENABLE_MPI:BOOL=", (build.with? "mpi"))
+    cmake_args << onoff("-DDOLFIN_ENABLE_PARMETIS:BOOL=", (build.with? "parmetis"))
+    cmake_args << onoff("-DDOLFIN_ENABLE_PASTIX:BOOL=", (build.with? "pastix"))
+
+    cmake_args << onoff("-DDOLFIN_ENABLE_PETSC:BOOL=", (build.with? "petsc"))
+    ENV["PETSC_DIR"] = "#{Formula["petsc"].opt_prefix}/real" if build.with? "petsc"
+
+    cmake_args << onoff("-DDOLFIN_ENABLE_SLEPC:BOOL=", (build.with? "slepc"))
+    ENV["SLEPC_DIR"] = "#{Formula["slepc"].opt_prefix}/real" if build.with? "slepc"
+
+    cmake_args << onoff("-DDOLFIN_ENABLE_SCOTCH:BOOL=", (build.with? "scotch"))
+    cmake_args << onoff("-DDOLFIN_ENABLE_TRILINOS:BOOL=", (build.with? "trilinos"))
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make"
+      system "make", "install"
     end
   end
 
