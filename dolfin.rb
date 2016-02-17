f51d6948df6f5ffe75efd9081df407661d393866
Dominique Orban <dominique.orban@gmail.com>
2016-02-17 18:17:49 -0500

    dolfin: add test

--- dolfin.rb
+++ dolfin.rb
@@ -97,4 +97,28 @@ class Dolfin < Formula
     Do not source dolfin.conf, as the DYLIB settings conflict with Homebrew.
     EOS
   end
+
+  test do
+    (testpath/"poisson.py").write <<-EOS.undent
+      from dolfin import *
+      mesh = UnitSquareMesh(32, 32)
+      V = FunctionSpace(mesh, "Lagrange", 1)
+
+      def boundary(x):
+        return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS
+
+      u0 = Constant(0.0)
+      bc = DirichletBC(V, u0, boundary)
+      u = TrialFunction(V)
+      v = TestFunction(V)
+      f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
+      g = Expression("sin(5*x[0])")
+      a = inner(grad(u), grad(v))*dx
+      L = f*v*dx + g*v*ds
+
+      u = Function(V)
+      solve(a == L, u, bc)
+    EOS
+    system "python", "poisson.py"
+  end
 end
