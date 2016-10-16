a6f5dc5d8883e848aa9ddc8c6d1b379af91e1b4d
Dominique Orban <dominique.orban@gmail.com>
2016-10-15 20:58:11 -0400

    install boost earlier and vtk without qt

--- .travis_helper/travis.sh
+++ .travis_helper/travis.sh
@@ -10,7 +10,7 @@ touch $BUILD_OUTPUT
 
 dump_output() {
    echo "############# Build log(last 1Mb) start #############"
-   tail -b 2048 $BUILD_OUTPUT
+   tail -b 1024 $BUILD_OUTPUT
    echo "############# Build log end #########################"
 }
 cleanup() {
@@ -27,10 +27,10 @@ trap error_handler ERR SIGHUP SIGINT SIGTERM
 # Set up a repeating loop to send some output to Travis.
 ( echo working; while true; do echo -n ...; sleep $PING_SLEEP; done; echo; ) &
 PING_LOOP_PID=$!
-echo "pid: $PING_LOOP_PID"
+echo "\e[33m[ping loop pid] $PING_LOOP_PID\e[0m"
 
 # Dump output of building dependencies to log file
-echo "executing: $@"
+echo "\e[33m[$@ > $BUILD_OUTPUT]\e[0m"
 $@ >> $BUILD_OUTPUT 2>&1
 
 # The build was successful. Clean up.
